aba1ecfee5c0dbadde8dc1e3df7d59f9e669e51c
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2021-09-30 11:23:48 +0200

    added create events page and event modal; restructured events and bookings backend schema

--- frontend/src/pages/events/Events.page.js
+++ frontend/src/pages/events/Events.page.js
@@ -1,8 +1,45 @@
 import React, { Component } from 'react';
 
+import Modal from '../../components/modal/Modal.component';
+import Backdrop from '../../components/backdrop/Backdrop.component';
+import './Events.styles.scss';
+
 class EventsPage extends Component {
+
+    state = {
+        creating: false
+    };
+
+    startCreateEventHandler = () => {
+        this.setState({ creating: true });
+    };
+
+    modalConfirmHandler = () => {
+        this.setState({ creating: false });
+    };
+
+    modalCancelHandler = () => {
+        this.setState({ creating: false });
+    };
+
     render() {
-        return (<h1>the events page</h1>)
+        return (
+            <React.Fragment>
+                {this.state.creating && <Backdrop />}
+                {this.state.creating &&
+                    <Modal
+                        title="Add Event"
+                        canCancel
+                        canConfirm
+                        onCancel={this.modalCancelHandler}
+                        onConfirm={this.modalConfirmHandler}>
+                        <p>Modal content</p>
+                    </Modal>}
+                <div className="events_page__items">
+                    <button onClick={this.startCreateEventHandler}>Create Event</button>
+                </div>
+            </React.Fragment>
+        )
     }
 }
 
